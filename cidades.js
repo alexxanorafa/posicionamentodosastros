52468653111fd3fa06022fe25d6de046ba17f8a2
Alexandre <86789074+alexxanorafa@users.noreply.github.com>
2025-01-23 03:25:29 +0000

    Add files via upload

---
+++ cidades.js
@@ -0,0 +1,253 @@
+const cidades = [
+    {
+      "cidade": "Tóquio",
+      "latitude": 35.6895,
+      "longitude": 139.6917
+    },
+    {
+      "cidade": "Pequim",
+      "latitude": 39.9042,
+      "longitude": 116.4074
+    },
+    {
+      "cidade": "Nova Délhi",
+      "latitude": 28.6139,
+      "longitude": 77.209
+    },
+    {
+      "cidade": "Nova York",
+      "latitude": 40.7128,
+      "longitude": -74.006
+    },
+    {
+      "cidade": "São Paulo",
+      "latitude": -23.5505,
+      "longitude": -46.6333
+    },
+    {
+      "cidade": "Xangai",
+      "latitude": 31.2304,
+      "longitude": 121.4737
+    },
+    {
+      "cidade": "Cidade do México",
+      "latitude": 19.4326,
+      "longitude": -99.1332
+    },
+    {
+      "cidade": "Osaka",
+      "latitude": 34.6937,
+      "longitude": 135.5023
+    },
+    {
+      "cidade": "Cairo",
+      "latitude": 30.0444,
+      "longitude": 31.2357
+    },
+    {
+      "cidade": "Bombaim (Mumbai)",
+      "latitude": 19.076,
+      "longitude": 72.8777
+    },
+    {
+      "cidade": "Dhaka",
+      "latitude": 23.8103,
+      "longitude": 90.4125
+    },
+    {
+      "cidade": "Karachi",
+      "latitude": 24.8607,
+      "longitude": 67.0011
+    },
+    {
+      "cidade": "Buenos Aires",
+      "latitude": -34.6037,
+      "longitude": -58.3816
+    },
+    {
+      "cidade": "Lagos",
+      "latitude": 6.5244,
+      "longitude": 3.3792
+    },
+    {
+      "cidade": "Cali",
+      "latitude": 3.4516,
+      "longitude": -76.532
+    },
+    {
+      "cidade": "Riyadh",
+      "latitude": 24.7136,
+      "longitude": 46.6753
+    },
+    {
+      "cidade": "Londres",
+      "latitude": 51.5074,
+      "longitude": -0.1278
+    },
+    {
+      "cidade": "Paris",
+      "latitude": 48.8566,
+      "longitude": 2.3522
+    },
+    {
+      "cidade": "Lima",
+      "latitude": -12.0464,
+      "longitude": -77.0428
+    },
+    {
+      "cidade": "Iémen (Sana)",
+      "latitude": 15.3694,
+      "longitude": 44.191
+    },
+    {
+      "cidade": "Madrid",
+      "latitude": 40.4168,
+      "longitude": -3.7038
+    },
+    {
+      "cidade": "Roma",
+      "latitude": 41.9028,
+      "longitude": 12.4964
+    },
+    {
+      "cidade": "Berlim",
+      "latitude": 52.52,
+      "longitude": 13.405
+    },
+    {
+      "cidade": "Lisboa",
+      "latitude": 38.7169,
+      "longitude": -9.1395
+    },
+    {
+      "cidade": "Porto",
+      "latitude": 41.1579,
+      "longitude": -8.6291
+    },
+    {
+      "cidade": "Barcelona",
+      "latitude": 41.3784,
+      "longitude": 2.1915
+    },
+    {
+      "cidade": "Amsterdão",
+      "latitude": 52.3676,
+      "longitude": 4.9046
+    },
+    {
+      "cidade": "Milão",
+      "latitude": 45.4642,
+      "longitude": 9.1900
+    },
+    {
+      "cidade": "Atenas",
+      "latitude": 37.9838,
+      "longitude": 23.7275
+    },
+    {
+      "cidade": "Viena",
+      "latitude": 48.2082,
+      "longitude": 16.3738
+    },
+    {
+      "cidade": "Bruxelas",
+      "latitude": 50.8503,
+      "longitude": 4.3517
+    },
+    // Capitais de Distrito Portuguesas
+    {
+      "cidade": "Aveiro",
+      "latitude": 40.6405,
+      "longitude": -8.6538
+    },
+    {
+      "cidade": "Beja",
+      "latitude": 38.5147,
+      "longitude": -7.8646
+    },
+    {
+      "cidade": "Braga",
+      "latitude": 41.5483,
+      "longitude": -8.4200
+    },
+    {
+      "cidade": "Bragança",
+      "latitude": 41.8056,
+      "longitude": -6.7589
+    },
+    {
+      "cidade": "Castelo Branco",
+      "latitude": 39.8186,
+      "longitude": -7.4992
+    },
+    {
+      "cidade": "Coimbra",
+      "latitude": 40.2136,
+      "longitude": -8.4265
+    },
+    {
+      "cidade": "Évora",
+      "latitude": 38.5739,
+      "longitude": -7.9137
+    },
+    {
+      "cidade": "Faro",
+      "latitude": 37.0194,
+      "longitude": -7.9304
+    },
+    {
+      "cidade": "Guarda",
+      "latitude": 40.5371,
+      "longitude": -7.2683
+    },
+    {
+      "cidade": "Leiria",
+      "latitude": 39.7431,
+      "longitude": -8.8070
+    },
+    {
+      "cidade": "Lisboa",
+      "latitude": 38.7169,
+      "longitude": -9.1395
+    },
+    {
+      "cidade": "Madeira (Funchal)",
+      "latitude": 32.6663,
+      "longitude": -16.9241
+    },
+    {
+      "cidade": "Portalegre",
+      "latitude": 39.2961,
+      "longitude": -7.4289
+    },
+    {
+      "cidade": "Porto",
+      "latitude": 41.1579,
+      "longitude": -8.6291
+    },
+    {
+      "cidade": "Santarém",
+      "latitude": 39.2366,
+      "longitude": -8.6875
+    },
+    {
+      "cidade": "Setúbal",
+      "latitude": 38.5247,
+      "longitude": -8.8882
+    },
+    {
+      "cidade": "Viana do Castelo",
+      "latitude": 41.6931,
+      "longitude": -8.8262
+    },
+    {
+      "cidade": "Vila Real",
+      "latitude": 41.3000,
+      "longitude": -7.7500
+    },
+    {
+      "cidade": "Viseu",
+      "latitude": 40.6613,
+      "longitude": -7.9121
+    }
+];
